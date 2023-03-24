eb3c5bb0018708cd0d0e5a0d87d4ddf9f472a859
nickbaldwin <nick.with.a.z@gmail.com>
2023-03-24 13:15:48 +1100

    post secret, change base

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./api/routes");
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.use("/api", routes);
+app.use("/", routes);
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
